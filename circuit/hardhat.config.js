22bca618bdc49e9924ea3312bb6b6a4551588cc7
FoodChain1028 <sh1001309@gmail.com>
2023-05-21 16:18:54 +0800

    add circuit

---
+++ circuit/hardhat.config.js
@@ -0,0 +1,27 @@
+require("hardhat-circom");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.11",
+      },
+      {
+        version: "0.8.9",
+      },
+    ],
+  },
+  circom: {
+    inputBasePath: "./circuits",
+    ptau: "https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau",
+    circuits: [
+      {
+        name: "Pedersen",
+        protocol: "plonk"
+      }
+    ],
+  },
+};
