434803a7decab1bcadefcfe05b437cc1a5c22c06
FoodChain1028 <sh1001309@gmail.com>
2023-05-24 16:08:45 +0800

    complete vote page

--- frontend/pages/voting.js
+++ frontend/pages/voting.js
@@ -1,16 +1,66 @@
 import React, { useState } from 'react'
 import { VotingCard, VotingCon } from './style'
-import Alice from '../asset/Alice.jpg'
-import Bob from '../asset/Bob.jpg'
-import John from '../asset/John.jpg'
+import { useContract, useContractWrite, useContractRead, Web3Button } from '@thirdweb-dev/react';
+import { Sepolia } from '@thirdweb-dev/chains';
+import { ThirdwebSDK } from '@thirdweb-dev/sdk';
+import api from '../apis/api';
 
+const voteAddress = "0x481D802837C0f97a45e47b8DfB5B3dD342Cd1147"
+const zkSBTAddress = "0x5041acf51104EbC59192D1fCf705F3adECb3D2FA"
+const verifierAddress = "0xC8df7B6e803A56EC45cbbA46385219fAdDFD7483"
 
 const Voting = () => {
   let J = '0x0bfa36c40b8771f59912a8b06e3ba9cd68504e69345a0ebcb952c3c6100ec88e'
   let A = '0x6070f87e7650727769f301b1e264c58d77a49792dc17c13fe3cb44a9bb1f7b44'
   let B = '0x780641b8ceca510c40f5f0178d126444811cc3e3edf7fa86f3656f77615dcc5c'
-  const [selection, setSelect] = useState("")
-  const [data, setData] = useState("")
+  const [selection, setSelect] = useState("");
+  const [candidate, setCandidate] = useState("");
+  const [secret, setSecret] = useState();
+  const [soulAddress, setSoulAddr] = useState("");
+  const [input, setInput] = useState([]);
+
+
+  const sdk = new ThirdwebSDK(Sepolia);
+
+  const { contract } = useContract(voteAddress);
+  const { mutateAsync: voteForCandidate } = useContractWrite(
+    contract,
+    "voteForCandidate"
+  )
+
+
+  const generateInput = async (_soul) => {
+    const contract = await sdk.getContract(zkSBTAddress);
+    const data = await contract.call("getPublicNum", [_soul])
+    const publicNum = parseInt(data._hex)
+    console.log(publicNum);
+    const proof = await api.generateCallData(secret, publicNum);
+    setInput(proof.Input)
+  }
+
+  const vote = async (
+
+  ) => {
+    console.log(123123);
+
+    try {
+
+      const data = await voteForCandidate({
+        args: [
+          candidate,
+          soulAddress,
+          verifierAddress,
+          zkSBTAddress,
+          input
+        ]
+      });
+      alert("Successfully Voted!")
+      console.info("contract call successs", data);
+    } catch (err) {
+      console.error("contract call failure", err);
+    }
+
+  }
 
   const User = (props) => {
     let name = props.name
@@ -27,7 +77,7 @@ const Voting = () => {
             ( o.o )<br />
             {" > ^ <"}</p>
         </div>
-        <button onClick={() => { setSelect(name); setData(D) }}>Choose</button>
+        <button onClick={() => { setSelect(name); setCandidate(D) }}>Choose</button>
       </div>
     )
   }
@@ -41,10 +91,39 @@ const Voting = () => {
         <User name={"Bob"} D={B} />
       </VotingCard>
       <p>
-        Your Choice: {selection} 
+        Your Choice: {selection}
       </p>
-      <button >
-        確定投票
+      <input
+        onChange={(e) => setSoulAddr(e.target.value)}
+        placeholder='your soul address'
+      />
+      <br />
+      <input
+        onChange={(e) => setSecret(e.target.value)}
+        placeholder='your secret'
+      />
+      <br />
+      <button onClick={
+        async () => {
+          await generateInput(soulAddress)
+        }
+      }>
+        Generate Input
+      </button>
+      <br />
+      <button onClick={
+        () => {
+          vote()
+            .then((result) => {
+              console.log(result);
+            })
+            .catch((e) => {
+              console.log(e);
+            })
+        }
+      }
+      >
+        Vote
       </button>
     </VotingCon>
   )
